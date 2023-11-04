5a951b99266337f9a6e7f99ae82d7e678ae48af1
Jason Tse <jasont@star21.com.au>
2023-11-04 18:57:46 +1100

    Initial Commit

---
+++ Star21Mobile/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Star21Mobile
+//
+//  Created by Jason Tse on 4/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Star21Mobile/Star21MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Star21MobileApp.swift
+//  Star21Mobile
+//
+//  Created by Jason Tse on 4/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Star21MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
